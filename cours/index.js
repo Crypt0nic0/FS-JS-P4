ee580a5f0cb2290f5b2808dcc06ec232501e385b
Crypt0nic0 <n.greneche45@gmail.com>
2023-05-10 22:15:34 +0200

    XMLHttpRequest / Fetch

---
+++ cours/index.js
@@ -0,0 +1,32 @@
+//=====================XMLHttpRequest=========================
+
+function reqListener() {
+   // console.log(this.responseText);
+}
+
+let req = new XMLHttpRequest();
+req.onload = reqListener;
+//req.open('get', 'data.txt', true);
+//req.open('get','data.json',true);
+req.open('get','https://api.blablagues.net/?rub=blagues',true);
+req.send();
+
+
+
+//=====================FETCH=========================
+
+//  fetch("monlien","object d'options")
+//     .then((response) => {
+//      //response
+//     })
+//     .catch((error) => console.log(err));
+
+fetch('data.txt')
+    .then((res) => res.text())
+    .then((data) => console.log(data));
+
+
+fetch("data.json")
+    .then((res) => res.json())
+    .then((data) => console.log(data));
+
