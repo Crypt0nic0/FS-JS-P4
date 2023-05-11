e844fff3a0cb66443737c32670c689d9abe26ed4
Crypt0nic0 <n.greneche45@gmail.com>
2023-05-11 10:45:56 +0200

    method GET / POST / DELETE / UPDATE

--- cours/index.js
+++ cours/index.js
@@ -30,3 +30,45 @@ fetch("data.json")
     .then((res) => res.json())
     .then((data) => console.log(data));
 
+const myHeaders = new Headers();
+
+const init = {
+    method: "GET",
+    headers: myHeaders,
+    mode: "cors"
+}
+
+// CRUD => Create (POST), Read (GET), Update (PUT), Delete (DELETE)
+
+fetch("data.json", init)
+    .then((res) => console.log(res));
+
+
+const init2 = {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+        pseudo: "From Scratch",
+        message: "Yo les gens !"
+    }),
+    mode: "cors",
+    credentials: "same-origin"
+};
+
+const del2 = {
+    method: "DELETE",
+    mode: "cors",
+    credentials: "same-origin"
+};
+
+document.getElementById("formAdd").addEventListener("submit", () => {
+    fetch('http://localhost:3000/posts', init2)
+        .then((res) => console.log('data envoyée'));
+});
+
+document.getElementById("formDelete").addEventListener("submit", () => {
+    fetch('http://localhost:3000/posts/' + "1", del2)
+        .then(() => console.log('data supprimée !'));
+});
